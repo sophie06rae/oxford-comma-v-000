2d8f0fb1327267fdd3dbbc8542eb9595f1720e8b
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-12 11:25:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+if array.length == 1
+  return array.join(" ")
+elsif array.length == 2
+  return array.join(" and ")
+elsif array.length == 3
+    array[0] << (',')
+    array[1] << (',')
+    array[-2] << (' and')
+    return array.join(" ")
+else array.length > 3
+  new_array = array[-2] << (", and ")
+  combind_array= array[0..-3] * (", ") + (", ") + new_array + array[-1]
+  return combind_array
+end
 
 end
