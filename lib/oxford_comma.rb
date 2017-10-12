26a54000e6d8552d4ed3d9cd241db9bc897449a3
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-12 12:36:02 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ elsif array.length == 2
   return array.join(" and ")
 elsif array.length == 3
     new_array = array[-2] << (', and ')
-    three_array = array[0..1] * (", ") + new_array + array[-1]
+    three_array = array[0] * (", ") + new_array + array[-1]
     return three_array
 else array.length > 3
   new_array = array[-2] << (", and ")
