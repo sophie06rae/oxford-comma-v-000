b6a60fdd09c559c4ebbafe3e2fc9352737561112
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-12 12:38:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,7 +9,7 @@ elsif array.length == 3
     return three_array
 else array.length > 3
   new_array = array[-2] << (", and ")
-  combind_array= array[0..-3] * (", ") + (", ") + new_array + array[-1]
-  return combind_array
+  three_or_more_array= array[0..-3] * (", ") + (", ") + new_array + array[-1]
+  return three_or_more_array
 end
 end
